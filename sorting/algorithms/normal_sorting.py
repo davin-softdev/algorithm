f512b2eed3c76e16a54253cb0e234003126e37f1
davin-softdev <vinh.ly.softdev@gmail.com>
2024-09-10 19:22:02 +0930

    update: add progress bar, time, and steps count for each algorithms.

--- sorting/algorithms/normal_sorting.py
+++ sorting/algorithms/normal_sorting.py
@@ -1,7 +1,18 @@
 from strategy import SortingStrategy
-
+from tqdm import tqdm  # Import tqdm for progress bar
 class NormalSorting(SortingStrategy):
+    def __init__(self):
+        self.steps = 0
+
     def sort(self, data):
-        data = list(data)
-        data.sort()
+        self.steps = 0
+        # Implementing Bubble Sort with tqdm progress bar
+        n = len(data)
+        # Add a progress bar for the outer loop
+        for i in tqdm(range(n), desc="Sorting Progress", unit="iteration"):
+            for j in range(0, n-i-1):
+                self.steps += 1  # Count the comparison
+                if data[j] > data[j + 1]:
+                    self.steps += 1  # Count the swap operation
+                    data[j], data[j + 1] = data[j + 1], data[j]
         return data
--- sorting/main.py
+++ sorting/main.py
@@ -1,1 +1,48 @@
 import time
+from context import SortContext
+from algorithms.normal_sorting import NormalSorting
+import random
+
+### generate random number data
+def generate_random_array(size=100, low=0, high=1000):
+    ### generate random array
+    ### size: size of array
+    ### low: lower bound
+    ### high: upper bound
+    return [random.randint(low, high) for _ in range(size)]
+
+### we need to measure time for each algorithm
+### sort_strategy: Sorting strategy to be used
+### data: data to be sorted
+def measure_time(sort_strategy, data):
+    ### measure time for each algorithm
+    ### sort_strategy: Sorting strategy to be used
+    ### data: data to be sorted
+
+    ## First: get the sorting context -> will according to sort_strategy arg
+    context = SortContext(sort_strategy)
+
+    ## start counting
+    start = time.time()
+
+    ## Second: call sort method
+    sorted_data = context.sort_data(data)
+
+    ## end counting
+    end = time.time()
+
+    elapsed_time = end - start
+
+    return sorted_data, elapsed_time, sort_strategy.steps
+
+def main():
+    data = generate_random_array(size=10000)
+
+    sorted_data, elapsed_time, steps = measure_time(NormalSorting(), data.copy())
+
+    print(f"NormalSorting: -> {elapsed_time}")
+
+    print(f"Steps: {steps}")
+
+if __name__ == "__main__":
+    main()
